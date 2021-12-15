d9b9b2d6a98740cc7d73f0d182c2ba5fbad75872
abdelc0 <90260021+abdelc0@users.noreply.github.com>
2021-12-15 14:32:39 +0100

    REFRENSIE
    YOOOOOOOOOOOOOOOOOOOOOO

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.01">
+    <title>utrecht pizza</title>
+    
+    <link href="script.js" rel="stylesheet">
+    <link rel="stylesheet" href="stylerefrensie.css">
+      <link rel="stylesheet" href="background_styles.css">
+  </head>
+  <body>
+    
+
+
+    
+    <header>
+      <nav class="navbar">
+        <div class="brand-title">soloshop</div>
+        <a href="#" class="toggle-button">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
+        <div class="navbar-links">
+          <ul>
+          <li><a href="index.html">Home</a></li>
+            <li><a href="informatie.html">infomatie</a></li>
+            <li><a href="index.php">referenties</a></li>
+            <li><a href="#">contact</a></li>
+            <li><a href="#">FAQ</a></li>
+        </ul>
+        </div>
+      </nav>
+      <h1>referenties</h1>
+      <h1>vul hier onder je mening over utrecht pizza</h1>
+      <div class="container">
+      <form action="create.php" method="post">
+      
+          <div class="firstname"> <label for="fname">First Name</label>
+          <input type="hallo" id="fname" name="firstname" placeholder="Your name..">
+      
+          <label for="lname">Last Name</label>
+          <input type="hallo" id="lname" name="lastname" placeholder="Your last name.."></div>
+          
+         <label for="lname">E-mail</label>
+          <input type="text" id="email" name="email" placeholder="Your last name..">
+      
+          
+      
+          <label for="subject">Subject</label>
+          <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+      
+          <input type="submit" value="Submit">
+      
+        </form>
+      </div>
+     
